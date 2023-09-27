5ffc850755ddc4c6f275a60bbcdd73199256d009
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-09-27 12:26:11 +0200

    test

--- Bojovani_Hrdy/MainWindow.xaml.cs
+++ Bojovani_Hrdy/MainWindow.xaml.cs
@@ -20,9 +20,53 @@ namespace Bojovani_Hrdy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private unit player1;
+        private unit player2;
+
         public MainWindow()
         {
             InitializeComponent();
+            player1 = new unit(500, 20);
+            player1.HitBox = new Rectangle();
+            player1.HitBox.Width = 40;
+            player1.HitBox.Height = 80;
+            
+
+            player2 = new unit(500, 20);
+            player2.HitBox = new Rectangle()
+            {
+                Width = 40,
+                Height = 80
+            };
+            DrawGame();
+            
+        }
+
+        private void DrawGame()
+        {
+            DrawTerrain();
+            DrawPlayer(player1); 
+            DrawPlayer(player2);
+        }
+
+        private void DrawPlayer()
+        {
+
+        }
+
+        private void DrawPlayer(unit player)
+        {
+
+        }
+
+        private void DrawTerrain()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = canvas.ActualWidth;
+            rectangle.Height = canvas.ActualHeight;
+            rectangle.Fill = Brushes.Green;
+            canvas.Children.Add(rectangle);
+
         }
     }
 }
---
+++ Bojovani_Hrdy/unit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace Bojovani_Hrdy
+{
+    public class unit
+    {
+        //private int hp;
+        //public int HP
+        //{
+        //    get => hp;
+        //    private set => Math.Max(Math.Min(value,1000),0);
+        //}
+
+        public int HP { get; private set; }
+        public int DMG { get; internal set; } = 10;
+        public int Angle { get; set; } = 0;
+        public int Speed { get; set; } = 0;
+        public Rectangle HitBox { get; set; } = new Rectangle();
+
+        public unit(int hp, int dmg)
+        {
+            HP = hp;
+            DMG = dmg;
+        }
+    }
+}
